d1fc0bbc01dd5b69a2217f01508cfdbe9973c08f
Pingfury <pingfury@outlook.com>
2021-08-27 23:59:25 +0800

    fix

--- cmd/root.go
+++ cmd/root.go
@@ -31,13 +31,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "happyhr",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A happy hr",
+	Long:  `A happy hr.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -56,15 +51,7 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.happyhr.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./happyhr.toml)")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -75,8 +62,8 @@ func initConfig() {
 	} else {
 
 		// Search config in home directory with name ".happyhr" (without extension).
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".happyhr")
+		viper.SetConfigType("toml")
+		viper.SetConfigName("happyhr")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
