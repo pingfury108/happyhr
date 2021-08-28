5a8623b918c4fe144dabe963d30ca4956d919ca0
Pingfury <pingfury@outlook.com>
2021-08-28 19:37:43 +0800

    fix views

--- router/router.go
+++ router/router.go
@@ -1,15 +1,23 @@
 package router
 
 import (
+	"happyhr/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
+var route *gin.Engine = gin.Default()
+
 func Create_gin() *gin.Engine {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	route.LoadHTMLGlob("views/*")
+
+	route.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	return r
+
+	route.GET("/", controllers.Index)
+	return route
 }
