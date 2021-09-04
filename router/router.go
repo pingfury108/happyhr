f194e88649e923961cf963e85653cb2962dc79d2
Pingfury <pingfury@outlook.com>
2021-09-04 15:54:34 +0800

    init web-ui

--- router/router.go
+++ router/router.go
@@ -1,8 +1,6 @@
 package router
 
 import (
-	"happyhr/controllers"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,14 +8,10 @@ var route *gin.Engine = gin.Default()
 
 func Create_gin() *gin.Engine {
 
-	route.LoadHTMLGlob("views/*")
-
 	route.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-
-	route.GET("/", controllers.Index)
 	return route
 }
