a691d114625668fade5a0a2aaf5567232bc8d371
Pingfury <pingfury@outlook.com>
2022-02-20 14:53:29 +0800

    employee curd ok

--- controllers/employee.go
+++ controllers/employee.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"happyhr/db"
 	"happyhr/models"
 
@@ -36,7 +37,14 @@ func GetALL(c *gin.Context) {
 
 func Query(c *gin.Context) {
 	var query metadata.QueryEmployee
-	c.Bind(&query)
+	if err := c.Bind(&query); err != nil {
+		log.Error(err)
+		c.JSON(400, metadata.QueryResult{
+			Msg:  fmt.Sprint(err),
+			Code: 400,
+			Data: []models.Employee{},
+		})
+	}
 	log.Debugf("bind query: %v", query)
 	if query.ID != 0 {
 		employee := models.Employee{}
@@ -105,25 +113,98 @@ func Query(c *gin.Context) {
 func Create(c *gin.Context) {
 	var employee models.Employee
 
-	if c.ShouldBindJSON(&employee) != nil {
-		log.Error("Parameter error")
+	if err := c.ShouldBindJSON(&employee); err != nil {
+		log.Error(err)
+		c.JSON(400, metadata.CreateResult{
+			Msg:  fmt.Sprint(err),
+			Code: 400,
+		})
+		return
 	}
 	log.Debug(employee)
 	if err := db.DB.Create(&employee).Error; err != nil {
 		log.Errorf("Create employee error: %v", err)
+		c.JSON(500, metadata.CreateResult{
+			Msg:  fmt.Sprint(err),
+			Code: 500,
+		})
+		return
 	}
+	c.JSON(200, metadata.CreateResult{
+		Msg:  "ok",
+		Code: 200,
+	})
 }
 
 func Update(c *gin.Context) {
+	//var from_employee metadata.UpdateEmployee
+	var from_employee models.Employee
+	var employee models.Employee
 
+	if err := c.ShouldBindJSON(&from_employee); err != nil {
+		log.Error(err)
+		c.JSON(400, metadata.UpdateResult{
+			Msg:  fmt.Sprint(err),
+			Code: 400,
+		})
+		return
+	}
+	log.Debug(from_employee)
+	if from_employee.ID == 0 {
+		c.JSON(400, metadata.UpdateResult{
+			Msg:  "employee id can not be empty",
+			Code: 400})
+		return
+	}
+	result := db.DB.First(&employee, from_employee.ID)
+	if result.Error != nil {
+		log.Error(result.Error)
+		return
+	}
+	db.DB.Model(&employee).Updates(&from_employee)
+	c.JSON(200, metadata.UpdateResult{
+		Msg:  "ok",
+		Code: 200,
+	})
 }
 
 func Delete(c *gin.Context) {
+	var parameter metadata.DeleteEmployee
+	var employee models.Employee
 
+	if err := c.Bind(&parameter); err != nil {
+		log.Error(err)
+		c.JSON(400, metadata.Result{
+			Msg:  fmt.Sprint(err),
+			Code: 400,
+		})
+	}
+	if parameter.ID == 0 {
+		c.JSON(400, metadata.Result{
+			Msg:  "employee id can not be empty",
+			Code: 400})
+		return
+	}
+	result := db.DB.First(&employee, parameter.ID)
+	if result.Error != nil {
+		log.Error(result.Error)
+		c.JSON(500, metadata.Result{
+			Msg:  fmt.Sprint(result.Error),
+			Code: 400,
+		})
+		return
+	}
+	db.DB.Delete(&employee)
+	c.JSON(200, metadata.Result{
+		Msg:  "ok",
+		Code: 200,
+	})
 }
 
 func init() {
 	router.Route.GET("/employees", GetALL)
 	router.Route.POST("/employees/query", Query)
 	router.Route.POST("/employees", Create)
+	router.Route.PUT("/employees", Update)
+	router.Route.DELETE("/employees", Delete)
 }
--- metadata/employee.go
+++ metadata/employee.go
@@ -1,6 +1,13 @@
 package metadata
 
-import "happyhr/models"
+import (
+	"happyhr/models"
+)
+
+type Result struct {
+	Msg  string `json:"msg"`
+	Code int    `json:"code"`
+}
 
 type QueryEmployee struct {
 	ID           uint   `from:"id"`
@@ -12,4 +19,18 @@ type QueryResult struct {
 	Msg  string            `json:"msg"`
 	Code int               `json:"code"`
 	Data []models.Employee `json:"data"`
+}
+
+type CreateResult struct {
+	Msg  string `json:"msg"`
+	Code int    `json:"code"`
+}
+
+type UpdateResult struct {
+	Msg  string `json:"msg"`
+	Code int    `json:"code"`
+}
+
+type DeleteEmployee struct {
+	ID uint `from:"id"`
 }
